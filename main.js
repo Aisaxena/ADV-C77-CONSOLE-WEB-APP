4db9014a83fc9c0cb50c2825e59ace116880a9ac
Avi-saxena <77326744+Avi-saxena@users.noreply.github.com>
2021-02-09 18:54:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Hey This Is My Console Project :D");
+
+
+
+
+var a = "Mine"
+var b = "Craft"
+var c = a.concat(b);
+console.log(a);
+console.log(b);
+console.log(c);
+var number1 = 4;
+var number2 = 1;
+var addition = number1 + number2;
+var subtraction = number1 - number2;
+var multiplication = number1 * number2;
+var division = number1 / number2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
